e155328378529d5fbfee5ba7640df239c1ddd7c1
shubham-vishwakarma10 <109148849+shubham-vishwakarma10@users.noreply.github.com>
2023-01-02 12:47:56 +0530

    My College Practical

---
+++ Practical1.R
@@ -0,0 +1,6 @@
+height<-c(102,117,105,141,135,115,138,144,157,100,131,119,115,121,113)
+weight<-c(61,46,62,54,60,69,51,50,46,64,48,56,64,48,59)
+student<-lm(weight~height)
+student
+predict(student,data,frame(height=199),interval="confidence")
+plot(student)
