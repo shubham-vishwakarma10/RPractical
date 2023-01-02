7b2dec7d7343977a209801bd2dfc90d5d583e9ae
shubham-vishwakarma10 <109148849+shubham-vishwakarma10@users.noreply.github.com>
2023-01-02 12:48:56 +0530

    Update Practical3.R

--- Practical3.R
+++ Practical3.R
@@ -1,4 +1,3 @@
-#Get the data points in form of a R vector.
 rainfall<-c(799,1174.8,865.1,1334.6,635.4,918.5,685.5,998.6,784.2,985,882.8,1071)
 
 rainfall.timeseries<-ts(rainfall,start = c(2012,1),frequency = 12)
